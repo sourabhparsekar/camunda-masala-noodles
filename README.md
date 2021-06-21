8d34deffe103d53f5766e09ab30794696ee1723f
sourabh_parsekar <sourabhanant@gmail.com>
2021-06-21 23:28:52 +0530

    Initial Commit - License, Flow, Skeleton

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cook Veg Masala Noodles using Camunda Workflow
+
+
+## Flow: 
+
+Veg Masala Noodles Cooking : 
+![Noodles Cooking Workflow Process](./src/main/resources/cook_noodles_process.png)
---
+++ src/main/java/com/noodles/MasalaNoodlesApplication.java
@@ -0,0 +1,13 @@
+package com.noodles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasalaNoodlesApplication {
+
+  public static void main(String... args) {
+    SpringApplication.run(MasalaNoodlesApplication.class, args);
+  }
+
+}
