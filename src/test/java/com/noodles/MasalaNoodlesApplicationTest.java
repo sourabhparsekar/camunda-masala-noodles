26af878c93bbac2ed203bccf77e03daafbb370c6
sourabh_parsekar <sourabhanant@gmail.com>
2021-09-30 01:41:18 +0530

    Fix Broken Test, Readme
    Signed-off-by: sourabh_parsekar <sourabhanant@gmail.com>

--- src/test/java/com/noodles/MasalaNoodlesApplicationTest.java
+++ src/test/java/com/noodles/MasalaNoodlesApplicationTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MasalaNoodlesApplicationTest {
 
